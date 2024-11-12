cdd0c9ae3cea1263b06a9b9a8e23502443e8acab
ara3isc <m.aneddame01@gmail.com>
2024-11-12 23:21:56 +0100

    3.0

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',  // Make Vite listen on all interfaces
+    port: 5173,        // Ensure the port matches the one in your Dockerfile
+  }
 })
